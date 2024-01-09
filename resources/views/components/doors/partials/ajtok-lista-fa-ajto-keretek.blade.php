3ca773fefa81c2049f753444a4b5eeaa693f9720
Cegem-360 <144362694+Cegem-360@users.noreply.github.com>
2024-01-09 12:31:54 +0100

    single product page design

---
+++ resources/views/components/doors/partials/ajtok-lista-fa-ajto-keretek.blade.php
@@ -0,0 +1,93 @@
+<ul class="products-list grid grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4">
+    <li class="products-list__tile">
+        <a href="/ajtok/adjustable-door-frame/">
+            <div class="tile">
+                <figure class="tile__image">
+                    <img src="{{ Vite::asset('resources/img/prd-images/thumbnails/Regulowana_0001_Oscieznica-regulowana-1-400x270.jpg') }}" alt="Adjustable door frame">
+                </figure>
+                <h3 class="tile__header hyphenate">Adjustable door frame</h3>
+                <div class="tile__icon">
+                    <x-doors.partials.arrow-right />
+                </div>
+            </div>
+        </a>
+    </li>
+    <li class="products-list__tile">
+        <a href="/ajtok/adjustable-non-rebated-door-frame/">
+            <div class="tile">
+                <figure class="tile__image">
+                    <img src="{{ Vite::asset('resources/img/prd-images/thumbnails/RB-_0000_regulowana-bezprzylgowa-1-400x270.jpg') }}" alt="Adjustable non-rebated door frame">
+                </figure>
+                <h3 class="tile__header hyphenate">Adjustable non-rebated door frame</h3>
+                <div class="tile__icon">
+                    <x-doors.partials.arrow-right />
+                </div>
+            </div>
+        </a>
+    </li>
+    <li class="products-list__tile">
+        <a href="/ajtok/new-arcus-door-frame/">
+            <div class="tile">
+                <figure class="tile__image">
+                    <img src="{{ Vite::asset('resources/img/prd-images/thumbnails/New-Arcus-_0004_New-Arcus-1-400x270.jpg') }}" alt="New Arcus door frame">
+                </figure>
+                <h3 class="tile__header hyphenate">New Arcus door frame</h3>
+                <div class="tile__icon">
+                    <x-doors.partials.arrow-right />
+                </div>
+            </div>
+        </a>
+    </li>
+    <li class="products-list__tile">
+        <a href="/ajtok/arcus-door-frame/">
+            <div class="tile">
+                <figure class="tile__image">
+                    <img src="{{ Vite::asset('resources/img/prd-images/thumbnails/Arcus_g1-1-400x270.jpg') }}" alt="Arcus door frame">
+                </figure>
+                <h3 class="tile__header hyphenate">Arcus door frame</h3>
+                <div class="tile__icon">
+                    <x-doors.partials.arrow-right />
+                </div>
+            </div>
+        </a>
+    </li>
+    <li class="products-list__tile">
+        <a href="/ajtok/hybrid/">
+            <div class="tile">
+                <figure class="tile__image">
+                    <img src="{{ Vite::asset('resources/img/prd-images/thumbnails/HYBRYDA-400x270.jpg') }}" alt="Hybrid">
+                </figure>
+                <h3 class="tile__header hyphenate">Hybrid</h3>
+                <div class="tile__icon">
+                    <x-doors.partials.arrow-right />
+                </div>
+            </div>
+        </a>
+    </li>
+    <li class="products-list__tile">
+        <a href="/ajtok/standard-2-pack-door-frame/">
+            <div class="tile">
+                <figure class="tile__image">
+                    <img src="{{ Vite::asset('resources/img/prd-images/thumbnails/Standard_2-pack_0004_Oscieznica-Standard-2-pack-1-e1516820852561-400x270.jpg') }}" alt="Standard 2-Pack door frame">
+                </figure>
+                <h3 class="tile__header hyphenate">Standard 2-Pack door frame</h3>
+                <div class="tile__icon">
+                    <x-doors.partials.arrow-right />
+                </div>
+            </div>
+        </a>
+    </li>
+    <li class="products-list__tile">
+        <a href="/ajtok/cmok-door-frame/">
+            <div class="tile">
+                <figure class="tile__image">
+                    <img src="{{ Vite::asset('resources/img/prd-images/thumbnails/CMOK-_0004_CMOK-1-400x270.jpg') }}" alt="CMOK door frame">
+                </figure>
+                <h3 class="tile__header hyphenate">CMOK door frame</h3>
+                <div class="tile__icon">
+                    <x-doors.partials.arrow-right />
+                </div>
+            </div>
+        </a>
+    </li>
+</ul>
---
+++ resources/views/components/doors/partials/ajtok-lista-furner-ajtok.blade.php
@@ -0,0 +1,249 @@
+<ul class="products-list grid grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4">
+    <li class="products-list__tile">
+        <a href="/ajtok/glas/">
+            <div class="tile">
+                <figure class="tile__image">
+                    <img src="{{ Vite::asset('resources/img/prd-images/thumbnails/classen_glas-400x270.jpg') }}" alt="Glas">
+                </figure>
+                <h3 class="tile__header hyphenate">Glas</h3>
+                <div class="tile__icon">
+                    <x-doors.partials.arrow-right />
+                </div>
+            </div>
+        </a>
+    </li>
+    <li class="products-list__tile">
+        <a href="/ajtok/modul/">
+            <div class="tile">
+                <figure class="tile__image">
+                    <img src="{{ Vite::asset('resources/img/prd-images/thumbnails/classen_modul-400x270.jpg') }}" alt="Modul">
+                </figure>
+                <h3 class="tile__header hyphenate">Modul</h3>
+                <div class="tile__icon">
+                    <x-doors.partials.arrow-right />
+                </div>
+            </div>
+        </a>
+    </li>
+    <li class="products-list__tile">
+        <a href="/ajtok/milk/">
+            <div class="tile">
+                <figure class="tile__image">
+                    <img src="{{ Vite::asset('resources/img/prd-images/thumbnails/milk_classen-400x270.jpg') }}" alt="Milk">
+                </figure>
+                <h3 class="tile__header hyphenate">Milk</h3>
+                <div class="tile__icon">
+                    <x-doors.partials.arrow-right />
+                </div>
+            </div>
+        </a>
+    </li>
+    <li class="products-list__tile">
+        <a href="/ajtok/verti/">
+            <div class="tile">
+                <figure class="tile__image">
+                    <img src="{{ Vite::asset('resources/img/prd-images/thumbnails/classen_verti-400x270.jpg') }}" alt="Verti">
+                </figure>
+                <h3 class="tile__header hyphenate">Verti</h3>
+                <div class="tile__icon">
+                    <x-doors.partials.arrow-right />
+                </div>
+            </div>
+        </a>
+    </li>
+    <li class="products-list__tile">
+        <a href="/ajtok/dark/">
+            <div class="tile">
+                <figure class="tile__image">
+                    <img src="{{ Vite::asset('resources/img/prd-images/thumbnails/dark_classen-400x270.jpg') }}" alt="Dark">
+                </figure>
+                <h3 class="tile__header hyphenate">Dark</h3>
+                <div class="tile__icon">
+                    <x-doors.partials.arrow-right />
+                </div>
+            </div>
+        </a>
+    </li>
+    <li class="products-list__tile">
+        <a href="/ajtok/saga/">
+            <div class="tile">
+                <figure class="tile__image">
+                    <img src="{{ Vite::asset('resources/img/prd-images/thumbnails/Classen_Saga-400x270.jpg') }}" alt="Saga">
+                </figure>
+                <h3 class="tile__header hyphenate">Saga</h3>
+                <div class="tile__icon">
+                    <x-doors.partials.arrow-right />
+                </div>
+            </div>
+        </a>
+    </li>
+    <li class="products-list__tile">
+        <a href="/ajtok/asteria-nl/">
+            <div class="tile">
+                <figure class="tile__image">
+                    <img src="{{ Vite::asset('resources/img/prd-images/thumbnails/Asteria-NL-400x270.jpg') }}" alt="Asteria NL">
+                </figure>
+                <h3 class="tile__header hyphenate">Asteria NL</h3>
+                <div class="tile__icon">
+                    <x-doors.partials.arrow-right />
+                </div>
+            </div>
+        </a>
+    </li>
+    <li class="products-list__tile">
+        <a href="/ajtok/discovery/">
+            <div class="tile">
+                <figure class="tile__image">
+                    <img src="{{ Vite::asset('resources/img/prd-images/thumbnails/Classen-drzwi-Discovery-Dab-kendal-400x270.jpg') }}" alt="Discovery">
+                </figure>
+                <h3 class="tile__header hyphenate">Discovery</h3>
+                <div class="tile__icon">
+                    <x-doors.partials.arrow-right />
+                </div>
+            </div>
+        </a>
+    </li>
+    <li class="products-list__tile">
+        <a href="/ajtok/tetyda/">
+            <div class="tile">
+                <figure class="tile__image">
+                    <img src="{{ Vite::asset('resources/img/prd-images/thumbnails/Tetyda-model-2-400x270.jpg') }}" alt="Tetyda">
+                </figure>
+                <h3 class="tile__header hyphenate">Tetyda</h3>
+                <div class="tile__icon">
+                    <x-doors.partials.arrow-right />
+                </div>
+            </div>
+        </a>
+    </li>
+    <li class="products-list__tile">
+        <a href="/ajtok/natura-hr-2/">
+            <div class="tile">
+                <figure class="tile__image">
+                    <img src="{{ Vite::asset('resources/img/prd-images/thumbnails/Natura-HR-1-400x270.jpg') }}" alt="Natura HR">
+                </figure>
+                <h3 class="tile__header hyphenate">Natura HR</h3>
+                <div class="tile__icon">
+                    <x-doors.partials.arrow-right />
+                </div>
+            </div>
+        </a>
+    </li>
+    <li class="products-list__tile">
+        <a href="/ajtok/andromeda/">
+            <div class="tile">
+                <figure class="tile__image">
+                    <img src="{{ Vite::asset('resources/img/prd-images/thumbnails/Andromeda-model-1-400x270.jpg') }}" alt="Andromeda">
+                </figure>
+                <h3 class="tile__header hyphenate">Andromeda</h3>
+                <div class="tile__icon">
+                    <x-doors.partials.arrow-right />
+                </div>
+            </div>
+        </a>
+    </li>
+    <li class="products-list__tile">
+        <a href="/ajtok/demeter/">
+            <div class="tile">
+                <figure class="tile__image">
+                    <img src="{{ Vite::asset('resources/img/prd-images/thumbnails/demeter-model-2-400x270.jpg') }}" alt="Demeter">
+                </figure>
+                <h3 class="tile__header hyphenate">Demeter</h3>
+                <div class="tile__icon">
+                    <x-doors.partials.arrow-right />
+                </div>
+            </div>
+        </a>
+    </li>
+    <li class="products-list__tile">
+        <a href="/ajtok/arena/">
+            <div class="tile">
+                <figure class="tile__image">
+                    <img src="{{ Vite::asset('resources/img/prd-images/thumbnails/ARENA--400x270.jpg') }}" alt="Arena">
+                </figure>
+                <h3 class="tile__header hyphenate">Arena</h3>
+                <div class="tile__icon">
+                    <x-doors.partials.arrow-right />
+                </div>
+            </div>
+        </a>
+    </li>
+    <li class="products-list__tile">
+        <a href="/ajtok/century/">
+            <div class="tile">
+                <figure class="tile__image">
+                    <img src="{{ Vite::asset('resources/img/prd-images/thumbnails/Century-400x270.jpg') }}" alt="Century">
+                </figure>
+                <h3 class="tile__header hyphenate">Century</h3>
+                <div class="tile__icon">
+                    <x-doors.partials.arrow-right />
+                </div>
+            </div>
+        </a>
+    </li>
+    <li class="products-list__tile">
+        <a href="/ajtok/malaga/">
+            <div class="tile">
+                <figure class="tile__image">
+                    <img src="{{ Vite::asset('resources/img/prd-images/thumbnails/Classen-Malaga-4-4-400x270.jpg') }}" alt="Malaga">
+                </figure>
+                <h3 class="tile__header hyphenate">Malaga</h3>
+                <div class="tile__icon">
+                    <x-doors.partials.arrow-right />
+                </div>
+            </div>
+        </a>
+    </li>
+    <li class="products-list__tile">
+        <a href="/ajtok/modena-4/">
+            <div class="tile">
+                <figure class="tile__image">
+                    <img src="{{ Vite::asset('resources/img/prd-images/thumbnails/Modena-4--400x270.jpg') }}" alt="Modena 4">
+                </figure>
+                <h3 class="tile__header hyphenate">Modena 4</h3>
+                <div class="tile__icon">
+                    <x-doors.partials.arrow-right />
+                </div>
+            </div>
+        </a>
+    </li>
+    <li class="products-list__tile">
+        <a href="/ajtok/natura/">
+            <div class="tile">
+                <figure class="tile__image">
+                    <img src="{{ Vite::asset('resources/img/prd-images/thumbnails/Classen-drzwi-Natura-biale-Classen-panele-Loft-cerama-Lahti-5000-400x270.jpg') }}" alt="Natura">
+                </figure>
+                <h3 class="tile__header hyphenate">Natura</h3>
+                <div class="tile__icon">
+                    <x-doors.partials.arrow-right />
+                </div>
+            </div>
+        </a>
+    </li>
+    <li class="products-list__tile">
+        <a href="/ajtok/antiope/">
+            <div class="tile">
+                <figure class="tile__image">
+                    <img src="{{ Vite::asset('resources/img/prd-images/thumbnails/Classen-drzwi-Antiope-jesion-bialy-Classen-panele-orest-Dab-Salerno-43523-400x270.jpg') }}" alt="Antiope">
+                </figure>
+                <h3 class="tile__header hyphenate">Antiope</h3>
+                <div class="tile__icon">
+                    <x-doors.partials.arrow-right />
+                </div>
+            </div>
+        </a>
+    </li>
+    <li class="products-list__tile">
+        <a href="/ajtok/temida/">
+            <div class="tile">
+                <figure class="tile__image">
+                    <img src="{{ Vite::asset('resources/img/prd-images/thumbnails/Temida--400x270.jpg') }}" alt="Temida">
+                </figure>
+                <h3 class="tile__header hyphenate">Temida</h3>
+                <div class="tile__icon">
+                    <x-doors.partials.arrow-right />
+                </div>
+            </div>
+        </a>
+    </li>
+</ul>
