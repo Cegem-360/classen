91d3f7258fac459fd456cbf1b5e6d82ef11a8b19
Cegem-360 <144362694+Cegem-360@users.noreply.github.com>
2024-01-10 14:04:25 +0100

    komponensbe szetszedes

---
+++ resources/views/components/nav/breadcrumb.blade.php
@@ -0,0 +1,16 @@
+<nav class="px-8 py-8">
+    <ul class="breadcrumbs__list">
+        <li class="breadcrumbs__item">
+            <a href="/" itemprop="item" class="home">Főoldal</a>
+        </li>
+        <li class="breadcrumbs__item">
+            <a href="/ajtok/" itemprop="item">Ajtók</a>
+        </li>
+        <li class="breadcrumbs__item">
+            <a href="/lakkozott-ajtok/" itemprop="item">Lakkozott ajtók</a>
+        </li>
+        <li class="breadcrumbs__item">
+            <strong>Lirgo</strong>
+        </li>
+    </ul>
+</nav>
