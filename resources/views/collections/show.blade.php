5b7fc24800c96038fa27f958fffce04f30fb3ab7
DCrepper <zoli.szabok@gmail.com>
2024-01-10 17:20:28 +0100

    Update show.blade.php

--- resources/views/collections/show.blade.php
+++ resources/views/collections/show.blade.php
@@ -1,7 +1,6 @@
 <x-app-layout>
     <x-collections.partials.collection-single-item :category=$category :doors=$doors :catalogs=$catalogs :tags=$tags />
     <x-nav.breadcrumb />
-
     <x-collections.partials.prd-gallery :cateory=$category />
     <!-- gap -->
     <div class="min-h-[60px]"></div>
