15457804cfbd8fee604b107b5f763467424926f1
Cegem-360 <144362694+Cegem-360@users.noreply.github.com>
2024-01-16 12:45:16 +0100

    door page

--- resources/views/doors/index.blade.php
+++ resources/views/doors/index.blade.php
@@ -1,4 +1,37 @@
 <x-layouts.app>
+    <x-nav.breadcrumb>
+        {{ $category->name }}
+    </x-nav.breadcrumb>
+
+    <x-collections.partials.door-model-selector :category='$category' />
+    <!-- gap -->
+    <div class="min-h-[60px]"></div>
+
+    <x-collections.partials.prd-gallery :category='$category' />
+    <!-- gap -->
+    <div class="min-h-[60px]"></div>
+
+    <x-collections.partials.door-model-icons :catalogs=$catalogs />
+
+    <!-- gap -->
+    <div class="min-h-[60px]"></div>
+    <x-collections.partials.specifications :door_specification='$category->door_specification' />
+
+    <!-- gap -->
+    <div class="min-h-[60px]"></div>
+
+    <x-collections.partials.technical-parameters :technical_parameter='$category->technical_parameter' :additional_options='$category->additional_options' />
+
+    <!-- gap -->
+    <div class="min-h-[60px]"></div>
+
+    <x-collections.partials.door-options :tags='$tags' :doors=$doors />
+
+    <x-footer.layout />
+
+
+
+
     <div class="flex pt-20" aria-label="Breadcrumb">
         <ol class="abj flex items-center" role="list">
             <li>
