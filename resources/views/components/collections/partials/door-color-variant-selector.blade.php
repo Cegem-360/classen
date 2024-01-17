39525e24a69ab49425b9ef0f6ef9cc1158876e04
DCrepper <zoli.szabok@gmail.com>
2024-01-17 13:43:56 +0100

    hibajavitás

--- resources/views/components/collections/partials/door-color-variant-selector.blade.php
+++ resources/views/components/collections/partials/door-color-variant-selector.blade.php
@@ -10,66 +10,102 @@
             <div class="door_fronts">
                 <div class="swiper lightbox3 door-color-variant-swiper w-full">
                     <div class="swiper-wrapper items-center">
-                        <div data-hash="slide1" class="swiper-slide">
-                            <a data-pswp-width="540" data-pswp-height="1200" href="{{ Vite::asset('resources/img/prd-images/ajtok/color-variants/glas_m-4_uni-white.jpg') }}" itemprop="contentUrl" target="_blank">
+                        <div class="swiper-slide" data-hash="slide1">
+                            <a data-pswp-width="540" data-pswp-height="1200"
+                                href="{{ Vite::asset('resources/img/prd-images/ajtok/color-variants/glas_m-4_uni-white.jpg') }}"
+                                itemprop="contentUrl" target="_blank">
                                 <div class="flex h-full w-full justify-center bg-[#ffffff00]">
-                                    <img class="max-h-[500px]" src="{{ Vite::asset('resources/img/prd-images/ajtok/color-variants/glas_m-4_uni-white.jpg') }}" alt="" itemprop="contentUrl" target="_blank">>
+                                    <img class="max-h-[500px]"
+                                        src="{{ Vite::asset('resources/img/prd-images/ajtok/color-variants/glas_m-4_uni-white.jpg') }}"
+                                        alt="" itemprop="contentUrl" target="_blank">
                                 </div>
                             </a>
                         </div>
-                        <div data-hash="slide2" class="swiper-slide">
-                            <a data-pswp-width="540" data-pswp-height="1200" href="{{ Vite::asset('resources/img/prd-images/ajtok/color-variants/glas_m-4_uni-black.jpg') }}" itemprop="contentUrl" target="_blank">
+                        <div class="swiper-slide" data-hash="slide2">
+                            <a data-pswp-width="540" data-pswp-height="1200"
+                                href="{{ Vite::asset('resources/img/prd-images/ajtok/color-variants/glas_m-4_uni-black.jpg') }}"
+                                itemprop="contentUrl" target="_blank">
                                 <div class="flex h-full w-full justify-center bg-[#ffffff00]">
-                                    <img class="max-h-[500px]" src="{{ Vite::asset('resources/img/prd-images/ajtok/color-variants/glas_m-4_uni-black.jpg') }}" alt="" itemprop="contentUrl" target="_blank">>
+                                    <img class="max-h-[500px]"
+                                        src="{{ Vite::asset('resources/img/prd-images/ajtok/color-variants/glas_m-4_uni-black.jpg') }}"
+                                        alt="" itemprop="contentUrl" target="_blank">
                                 </div>
                             </a>
                         </div>
-                        <div data-hash="slide3" class="swiper-slide">
-                            <a data-pswp-width="540" data-pswp-height="1200" href="{{ Vite::asset('resources/img/prd-images/ajtok/color-variants/glas_m4_akacja-jasna.jpg') }}" itemprop="contentUrl" target="_blank">
+                        <div class="swiper-slide" data-hash="slide3">
+                            <a data-pswp-width="540" data-pswp-height="1200"
+                                href="{{ Vite::asset('resources/img/prd-images/ajtok/color-variants/glas_m4_akacja-jasna.jpg') }}"
+                                itemprop="contentUrl" target="_blank">
                                 <div class="flex h-full w-full justify-center bg-[#ffffff00]">
-                                    <img class="max-h-[500px]" src="{{ Vite::asset('resources/img/prd-images/ajtok/color-variants/glas_m4_akacja-jasna.jpg') }}" alt="" itemprop="contentUrl" target="_blank">>
+                                    <img class="max-h-[500px]"
+                                        src="{{ Vite::asset('resources/img/prd-images/ajtok/color-variants/glas_m4_akacja-jasna.jpg') }}"
+                                        alt="" itemprop="contentUrl" target="_blank">
                                 </div>
                             </a>
                         </div>
-                        <div data-hash="slide4" class="swiper-slide">
-                            <a data-pswp-width="540" data-pswp-height="1200" href="{{ Vite::asset('resources/img/prd-images/ajtok/color-variants/glas_m4_dab-grigio.jpg') }}" itemprop="contentUrl" target="_blank">
+                        <div class="swiper-slide" data-hash="slide4">
+                            <a data-pswp-width="540" data-pswp-height="1200"
+                                href="{{ Vite::asset('resources/img/prd-images/ajtok/color-variants/glas_m4_dab-grigio.jpg') }}"
+                                itemprop="contentUrl" target="_blank">
                                 <div class="flex h-full w-full justify-center bg-[#ffffff00]">
-                                    <img class="max-h-[500px]" src="{{ Vite::asset('resources/img/prd-images/ajtok/color-variants/glas_m4_dab-grigio.jpg') }}" alt="" itemprop="contentUrl" target="_blank">>
+                                    <img class="max-h-[500px]"
+                                        src="{{ Vite::asset('resources/img/prd-images/ajtok/color-variants/glas_m4_dab-grigio.jpg') }}"
+                                        alt="" itemprop="contentUrl" target="_blank">
                                 </div>
                             </a>
                         </div>
-                        <div data-hash="slide5" class="swiper-slide">
-                            <a data-pswp-width="540" data-pswp-height="1200" href="{{ Vite::asset('resources/img/prd-images/ajtok/color-variants/glas_m4_dab-hamilton.jpg') }}" itemprop="contentUrl" target="_blank">
+                        <div class="swiper-slide" data-hash="slide5">
+                            <a data-pswp-width="540" data-pswp-height="1200"
+                                href="{{ Vite::asset('resources/img/prd-images/ajtok/color-variants/glas_m4_dab-hamilton.jpg') }}"
+                                itemprop="contentUrl" target="_blank">
                                 <div class="flex h-full w-full justify-center bg-[#ffffff00]">
-                                    <img class="max-h-[500px]" src="{{ Vite::asset('resources/img/prd-images/ajtok/color-variants/glas_m4_dab-hamilton.jpg') }}" alt="" itemprop="contentUrl" target="_blank">>
+                                    <img class="max-h-[500px]"
+                                        src="{{ Vite::asset('resources/img/prd-images/ajtok/color-variants/glas_m4_dab-hamilton.jpg') }}"
+                                        alt="" itemprop="contentUrl" target="_blank">
                                 </div>
                             </a>
                         </div>
-                        <div data-hash="slide6" class="swiper-slide">
-                            <a data-pswp-width="540" data-pswp-height="1200" href="{{ Vite::asset('resources/img/prd-images/ajtok/color-variants/glas_m4_dab-marone.jpg') }}" itemprop="contentUrl" target="_blank">
+                        <div class="swiper-slide" data-hash="slide6">
+                            <a data-pswp-width="540" data-pswp-height="1200"
+                                href="{{ Vite::asset('resources/img/prd-images/ajtok/color-variants/glas_m4_dab-marone.jpg') }}"
+                                itemprop="contentUrl" target="_blank">
                                 <div class="flex h-full w-full justify-center bg-[#ffffff00]">
-                                    <img class="max-h-[500px]" src="{{ Vite::asset('resources/img/prd-images/ajtok/color-variants/glas_m4_dab-marone.jpg') }}" alt="" itemprop="contentUrl" target="_blank">>
+                                    <img class="max-h-[500px]"
+                                        src="{{ Vite::asset('resources/img/prd-images/ajtok/color-variants/glas_m4_dab-marone.jpg') }}"
+                                        alt="" itemprop="contentUrl" target="_blank">
                                 </div>
                             </a>
                         </div>
-                        <div data-hash="slide7" class="swiper-slide">
-                            <a data-pswp-width="540" data-pswp-height="1200" href="{{ Vite::asset('resources/img/prd-images/ajtok/color-variants/glas_m4_dab-kendal.jpg') }}" itemprop="contentUrl" target="_blank">
+                        <div class="swiper-slide" data-hash="slide7">
+                            <a data-pswp-width="540" data-pswp-height="1200"
+                                href="{{ Vite::asset('resources/img/prd-images/ajtok/color-variants/glas_m4_dab-kendal.jpg') }}"
+                                itemprop="contentUrl" target="_blank">
                                 <div class="flex h-full w-full justify-center bg-[#ffffff00]">
-                                    <img class="max-h-[500px]" src="{{ Vite::asset('resources/img/prd-images/ajtok/color-variants/glas_m4_dab-kendal.jpg') }}" alt="" itemprop="contentUrl" target="_blank">>
+                                    <img class="max-h-[500px]"
+                                        src="{{ Vite::asset('resources/img/prd-images/ajtok/color-variants/glas_m4_dab-kendal.jpg') }}"
+                                        alt="" itemprop="contentUrl" target="_blank">
                                 </div>
                             </a>
                         </div>
-                        <div data-hash="slide8" class="swiper-slide">
-                            <a data-pswp-width="540" data-pswp-height="1200" href="{{ Vite::asset('resources/img/prd-images/ajtok/color-variants/glas_m4_jesion-norweski.jpg') }}" itemprop="contentUrl" target="_blank">
+                        <div class="swiper-slide" data-hash="slide8">
+                            <a data-pswp-width="540" data-pswp-height="1200"
+                                href="{{ Vite::asset('resources/img/prd-images/ajtok/color-variants/glas_m4_jesion-norweski.jpg') }}"
+                                itemprop="contentUrl" target="_blank">
                                 <div class="flex h-full w-full justify-center bg-[#ffffff00]">
-                                    <img class="max-h-[500px]" src="{{ Vite::asset('resources/img/prd-images/ajtok/color-variants/glas_m4_jesion-norweski.jpg') }}" alt="" itemprop="contentUrl" target="_blank">>
+                                    <img class="max-h-[500px]"
+                                        src="{{ Vite::asset('resources/img/prd-images/ajtok/color-variants/glas_m4_jesion-norweski.jpg') }}"
+                                        alt="" itemprop="contentUrl" target="_blank">
                                 </div>
                             </a>
                         </div>
-                        <div data-hash="slide9" class="swiper-slide">
-                            <a data-pswp-width="540" data-pswp-height="1200" href="{{ Vite::asset('resources/img/prd-images/ajtok/color-variants/glas_m4_szary-matt.jpg') }}" itemprop="contentUrl" target="_blank">
+                        <div class="swiper-slide" data-hash="slide9">
+                            <a data-pswp-width="540" data-pswp-height="1200"
+                                href="{{ Vite::asset('resources/img/prd-images/ajtok/color-variants/glas_m4_szary-matt.jpg') }}"
+                                itemprop="contentUrl" target="_blank">
                                 <div class="flex h-full w-full justify-center bg-[#ffffff00]">
-                                    <img class="max-h-[500px]" src="{{ Vite::asset('resources/img/prd-images/ajtok/color-variants/glas_m4_szary-matt.jpg') }}" alt="" itemprop="contentUrl" target="_blank">>
+                                    <img class="max-h-[500px]"
+                                        src="{{ Vite::asset('resources/img/prd-images/ajtok/color-variants/glas_m4_szary-matt.jpg') }}"
+                                        alt="" itemprop="contentUrl" target="_blank">
                                 </div>
                             </a>
                         </div>
@@ -81,29 +117,32 @@
             <div class="door_swatches">
                 <div class="min-h-[330px] flex-wrap items-center md:flex-col">
                     <div class="variant-group">
-                        <h3 class=" text-2xl font-bold">Iridium</h3>
-                        <div class="doors_colors__variant-set flex py-8 border-b-1 border-[#bfbfbf]">
-                            <div class="door_variant first:ml-0 mx-4 my-2">
+                        <h3 class="text-2xl font-bold">Iridium</h3>
+                        <div class="doors_colors__variant-set flex border-b-1 border-[#bfbfbf] py-8">
+                            <div class="door_variant mx-4 my-2 first:ml-0">
                                 <a class="block w-[94px]" href="#slide1">
-                                    <img class="w-[88px] h-[88px] rounded-full" src="{{ Vite::asset('resources/img/prd-images/ajtok/color-variants/swatches/ir-uni-white.jpg') }}">
+                                    <img class="h-[88px] w-[88px] rounded-full"
+                                        src="{{ Vite::asset('resources/img/prd-images/ajtok/color-variants/swatches/ir-uni-white.jpg') }}">
                                     <div class="door_variant__meta">
-                                        <h4 class="mt-2 text-sm text-center">UNI White</h4>
+                                        <h4 class="mt-2 text-center text-sm">UNI White</h4>
                                     </div>
                                 </a>
                             </div>
-                            <div class="door_variant first:ml-0 mx-4 my-2">
+                            <div class="door_variant mx-4 my-2 first:ml-0">
                                 <a class="block w-[94px]" href="#slide2">
-                                    <img class="w-[88px] h-[88px] rounded-full" src="{{ Vite::asset('resources/img/prd-images/ajtok/color-variants/swatches/czarny-9005.jpg') }}">
+                                    <img class="h-[88px] w-[88px] rounded-full"
+                                        src="{{ Vite::asset('resources/img/prd-images/ajtok/color-variants/swatches/czarny-9005.jpg') }}">
                                     <div class="door_variant__meta">
-                                        <h4 class="mt-2 text-sm text-center">UNI Black</h4>
+                                        <h4 class="mt-2 text-center text-sm">UNI Black</h4>
                                     </div>
                                 </a>
                             </div>
-                            <div class="door_variant first:ml-0 mx-4 my-2">
+                            <div class="door_variant mx-4 my-2 first:ml-0">
                                 <a class="block w-[94px]" href="#slide3">
-                                    <img class="w-[88px] h-[88px] rounded-full" src="{{ Vite::asset('resources/img/prd-images/ajtok/color-variants/swatches/dab-marone.jpg') }}">
+                                    <img class="h-[88px] w-[88px] rounded-full"
+                                        src="{{ Vite::asset('resources/img/prd-images/ajtok/color-variants/swatches/dab-marone.jpg') }}">
                                     <div class="door_variant__meta">
-                                        <h4 class="mt-2 text-sm text-center">Marone Oak</h4>
+                                        <h4 class="mt-2 text-center text-sm">Marone Oak</h4>
                                     </div>
                                 </a>
                             </div>
--- resources/views/components/collections/partials/prd-gallery.blade.php
+++ resources/views/components/collections/partials/prd-gallery.blade.php
@@ -1,22 +1,25 @@
    @props(['category' => ''])
-   <div class="mx-8 grid grid-cols-[3fr_1fr_2fr] md:grid-cols-1 gap-3">
+   <div class="mx-8 grid grid-cols-[3fr_1fr_2fr] gap-3 md:grid-cols-1">
        <div class="swiper detail-page-swiper lightbox w-full">
            <div class="swiper-wrapper">
                @foreach ($category->gallery_imgs as $img)
-               <div class="swiper-slide min-h-[380px] w-3/4 bg-cover bg-center sm:w-full" style="background-position: 40% 50%; background-image: url('{{ !empty($img) ? $img : Vite::asset('resources/img/placeholder.webp') }}');">
-                   <a data-pswp-width="1600" data-pswp-height="900" href="{{ !empty($img) ? $img : Vite::asset('resources/img/placeholder.webp') }} }}" itemprop="contentUrl" target="_blank">
-                       <div class="h-full w-full bg-[#ffffff00]"></div>
-                   </a>
-               </div>
+                   <div class="swiper-slide min-h-[380px] w-3/4 bg-cover bg-center sm:w-full"
+                       style="background-position: 40% 50%; background-image: url('{{ !empty($img) ? $img : Vite::asset('resources/img/placeholder.webp') }}');">
+                       <a data-pswp-width="1600" data-pswp-height="900"
+                           href="{{ !empty($img) ? $img : Vite::asset('resources/img/placeholder.webp') }}"
+                           itemprop="contentUrl" target="_blank">
+                           <div class="h-full w-full bg-[#ffffff00]"></div>
+                       </a>
+                   </div>
                @endforeach
            </div>
        </div>
-       <div class="swiper detail-page-swiper-thumbs w-full  md:w-1/3" thumbsSlider="">
-           <div class="swiper-wrapper flex-wrap !h-auto">
+       <div class="swiper detail-page-swiper-thumbs w-full md:w-1/3" thumbsSlider="">
+           <div class="swiper-wrapper !h-auto flex-wrap">
                @foreach ($category->gallery_imgs as $img)
-               <div class="swiper-slide">
-                   <img src=" {{ !empty($img) ? $img : Vite::asset('resources/img/placeholder.webp') }}" />
-               </div>
+                   <div class="swiper-slide">
+                       <img src=" {{ !empty($img) ? $img : Vite::asset('resources/img/placeholder.webp') }}" />
+                   </div>
                @endforeach
            </div>
        </div>
