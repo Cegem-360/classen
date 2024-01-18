12be8c4a8c0d10cf58f24ff3308b02bd57a0a5ee
DCrepper <zoli.szabok@gmail.com>
2024-01-18 16:22:15 +0100

    Update layout.blade.php

--- resources/views/components/doors/partials/doors-list/layout.blade.php
+++ resources/views/components/doors/partials/doors-list/layout.blade.php
@@ -6,7 +6,6 @@
     <div class="min-h-[60px]"></div>
     <div class="mx-8 grid grid-cols-4 gap-6 md:grid-cols-2 sm:grid-cols-1">
         @foreach ($doors as $door)
-            @dd($door)
             <div class="relative grid place-content-center bg-neutral-100 px-6 pb-2 pt-12">
                 <div class="lazy absolute right-0 top-0 mr-2 mt-2 h-[32px] w-[32px]"
                     style="background-position: 0% 50%; background-image: url('{{ Vite::asset('resources/img/icons/heart-sprite.png') }}');"
@@ -14,10 +13,10 @@
                 </div>
                 <a class="" href="{{ route('door.show', ['door' => $door->id]) }}" wire:navigate>
                     <img class="max-h-[330px]"
-                        src="{{ Vite::asset('resources/img/prd-images/ajtok/models/glas_m1.jpg') }}" />
+                        src="{{ !empty($door->img_url) ? $door->img_url : Vite::asset('resources/img/placeholder.webp') }}" />
                     <div class="w-full pt-2">
-                        <h4 class="font-bold">Glas - model 4</h4>
-                        <p>Gray Matt</p>
+                        <h4 class="font-bold">{{ $door->category->name }} - {{ $door->name }}</h4>
+                        <p>{{ $door->tag_name }}</p>
                     </div>
                 </a>
             </div>
