2a1be5eed990d61f96088a9505fb838b1038c15e
Cegem-360 <144362694+Cegem-360@users.noreply.github.com>
2024-01-18 14:37:47 +0100

    door lista

---
+++ resources/views/components/doors/partials/main-content.blade.php
@@ -0,0 +1,10 @@
+ @props(['collections' => []])
+ <div class="mx-12 my-16 w-3/4">
+     <h1 class="text-3xl font-bold uppercase">Összes ajtó</h1>
+     <hr class="relative mb-3 mt-8 h-1 w-72 border-0 bg-sarga">
+     <!-- gap -->
+     <div class="min-h-[60px]"></div>
+
+     <x-doors.partials.osszes-ajto />
+
+ </div>
---
+++ resources/views/components/doors/partials/osszes-ajto.blade.php
@@ -0,0 +1,62 @@
+   <div class="mx-8 grid grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-6">
+       <div class="grid place-content-center relative px-6 pt-12 pb-2 bg-neutral-100">
+      <div class="mt-2 mr-2 absolute top-0 right-0 w-[32px] h-[32px]" style="background-position: 0% 50%; background-image: url('{{ Vite::asset('resources/img/icons/heart-sprite.png') }}');"></div>
+           <a class="" href="1/">
+               <img class="max-h-[330px]" src="{{ Vite::asset('resources/img/prd-images/ajtok/models/glas_m1.jpg') }}" />
+               <div class="pt-2 w-full">
+                   <h4 class="font-bold">Glas - model 4</h4>
+                   <p>Gray Matt</p>
+               </div>
+           </a>
+       </div>
+       <div class="grid place-content-center relative px-6 pt-12 pb-2 bg-neutral-100">
+           <div class="mt-2 mr-2 absolute top-0 right-0 w-[32px] h-[32px]" style="background-position: 0% 50%; background-image: url('{{ Vite::asset('resources/img/icons/heart-sprite.png') }}');"></div>
+           <a class="" href="1/">
+               <img class="max-h-[330px]" src="{{ Vite::asset('resources/img/prd-images/ajtok/models/glas_m2.jpg') }}" />
+               <div class="pt-2 w-full">
+                   <h4 class="font-bold">Glas - model 4</h4>
+                   <p>Gray Matt</p>
+               </div>
+           </a>
+       </div>
+       <div class="grid place-content-center relative px-6 pt-12 pb-2 bg-neutral-100">
+          <div class="mt-2 mr-2 absolute top-0 right-0 w-[32px] h-[32px]" style="background-position: 0% 50%; background-image: url('{{ Vite::asset('resources/img/icons/heart-sprite.png') }}');"></div>
+           <a class="" href="1/">
+               <img class="max-h-[330px]" src="{{ Vite::asset('resources/img/prd-images/ajtok/models/glas_m3.jpg') }}" />
+               <div class="pt-2 w-full">
+                   <h4 class="font-bold">Glas - model 4</h4>
+                   <p>Gray Matt</p>
+               </div>
+           </a>
+       </div>
+       <div class="grid place-content-center relative px-6 pt-12 pb-2 bg-neutral-100">
+          <div class="mt-2 mr-2 absolute top-0 right-0 w-[32px] h-[32px]" style="background-position: 0% 50%; background-image: url('{{ Vite::asset('resources/img/icons/heart-sprite.png') }}');"></div>
+           <a class="" href="1/">
+               <img class="max-h-[330px]" src="{{ Vite::asset('resources/img/prd-images/ajtok/models/glas_m4.jpg') }}" />
+               <div class="pt-2 w-full">
+                   <h4 class="font-bold">Glas - model 4</h4>
+                   <p>Gray Matt</p>
+               </div>
+           </a>
+       </div>
+       <div class="grid place-content-center relative px-6 pt-12 pb-2 bg-neutral-100">
+          <div class="mt-2 mr-2 absolute top-0 right-0 w-[32px] h-[32px]" style="background-position: 0% 50%; background-image: url('{{ Vite::asset('resources/img/icons/heart-sprite.png') }}');"></div>
+           <a class="" href="1/">
+               <img class="max-h-[330px]" src="{{ Vite::asset('resources/img/prd-images/ajtok/models/glas_m5.jpg') }}" />
+               <div class="pt-2 w-full">
+                   <h4 class="font-bold">Glas - model 4</h4>
+                   <p>Gray Matt</p>
+               </div>
+           </a>
+       </div>
+       <div class="grid place-content-center relative px-6 pt-12 pb-2 bg-neutral-100">
+          <div class="mt-2 mr-2 absolute top-0 right-0 w-[32px] h-[32px]" style="background-position: 0% 50%; background-image: url('{{ Vite::asset('resources/img/icons/heart-sprite.png') }}');"></div>
+           <a class="" href="1/">
+               <img class="max-h-[330px]" src="{{ Vite::asset('resources/img/prd-images/ajtok/models/glas_m6.jpg') }}" />
+               <div class="pt-2 w-full">
+                   <h4 class="font-bold">Glas - model 4</h4>
+                   <p>Gray Matt</p>
+               </div>
+           </a>
+       </div>
+   </div>
