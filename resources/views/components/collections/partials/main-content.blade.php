df34989192b7451261a8a4a95b5543356e4a9b93
DCrepper <zoli.szabok@gmail.com>
2024-01-23 10:40:08 +0100

    Nyelvesítés

--- resources/views/components/collections/partials/main-content.blade.php
+++ resources/views/components/collections/partials/main-content.blade.php
@@ -1,12 +1,12 @@
  @props(['collections' => []])
  <div class="mx-12 my-16 w-3/4">
-     <h1 class="text-3xl font-bold uppercase">Ajtók</h1>
+     <h1 class="text-3xl font-bold uppercase">Kollekció</h1>
 
      <!-- gap -->
      <div class="min-h-[60px]"></div>
      @foreach ($collections as $head => $items)
          <hr class="relative mb-3 mt-8 h-1 w-72 border-0 bg-sarga">
-         <h2 class="mb-3 text-2xl uppercase">Lakkozott ajtók</h2>
+         <h2 class="mb-3 text-2xl uppercase">{{ __($head) }}</h2>
          {{-- title = tag value --}}
          <ul class="products-list grid grid-cols-3 gap-4 md:grid-cols-2 sm:grid-cols-1">
              @foreach ($items as $item)
