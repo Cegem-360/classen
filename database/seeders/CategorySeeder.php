1bdbe4e818b0f7ec8caf6e3fec7d087f72fdb2c4
DCrepper <zoli.szabok@gmail.com>
2024-02-02 12:06:43 +0100

    Update CategorySeeder.php

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -53,11 +53,11 @@ class CategorySeeder extends Seeder
             $gallery_imgs = null;
 
 
-            $additional_options     = ($categories['acf']['additional_options']) ? $categories['acf']['additional_options'] : null;
+            $additional_options = ($categories['acf']['additional_options']) ? $categories['acf']['additional_options'] : null;
             $additional_options = $this->str_replace_json('false', 'null', $additional_options);
-            $door_specification     = ($categories['acf']['door_specification']) ? $categories['acf']['door_specification'] : null;
+            $door_specification = ($categories['acf']['door_specification']) ? $categories['acf']['door_specification'] : null;
             $door_specification = $this->str_replace_json('false', 'null', $door_specification);
-            $technical_parameter    = ($categories['acf']['technical_parameter']) ? $categories['acf']['technical_parameter'] : null;
+            $technical_parameter = ($categories['acf']['technical_parameter']) ? $categories['acf']['technical_parameter'] : null;
             $technical_parameter = $this->str_replace_json('false', 'null', $technical_parameter);
             $gallery_imgs = $categories['acf']['galeria_kepek'] ?? null;
             $img = $categories['acf']['galeria_kepek'][0] ?? null;
@@ -68,7 +68,7 @@ class CategorySeeder extends Seeder
                     'additional_options' => $additional_options,
                     'door_specification' => $door_specification,
                     'technical_parameter' => $technical_parameter,
-                    'img_url' =>  $img,
+                    'img_url' => $img,
                     'gallery_imgs' => $gallery_imgs,
                     'breadcrumb' => $categories['acf']['type'],
                 ]
@@ -77,9 +77,10 @@ class CategorySeeder extends Seeder
 
             $attributes = explode("|", $woocommerceCategory->description);
             foreach ($attributes as $attribute) {
-                if (is_null($attribute)) continue;
+                if (is_null($attribute))
+                    continue;
                 if (isset($attribute) && $attribute != "") {
-                    $attribute =  Attribute::whereName($attribute)->first();
+                    $attribute = Attribute::whereName($attribute)->first();
                     $created_category->attributes()->attach($attribute);
                 }
             }
@@ -88,9 +89,9 @@ class CategorySeeder extends Seeder
             foreach ($products as $product) {
                 $cover = $product->images[0]->src;
                 try {
-                    $tag = Cache::remember("tag_" . $product->tags[0]->name, 1, function () use ($woocommerce, $product) {
-                        $tag = $woocommerce->get(EndPoint::PRODUCTSTAGS, ['search' => $product->tags[0]->name]);
 
+                    $tag = Cache::remember("tag_slug_" . $product->tags[0]->slug, 1, function () use ($woocommerce, $product) {
+                        $tag = $woocommerce->get(EndPoint::PRODUCTSTAGS, ['slug' => $product->tags[0]->slug]);
                         return $tag;
                     });
                 } catch (\Throwable $th) {
@@ -112,7 +113,7 @@ class CategorySeeder extends Seeder
                     'category_id' => $woocommerceCategory->id,
                     'tag' => $tag->slug,
                     'tag_name' => $tag->name,
-                    'tag_img_url' =>  $exploded[0],
+                    'tag_img_url' => $exploded[0],
                     'tag_category' => $exploded[1]
                 ]);
             }
@@ -120,6 +121,6 @@ class CategorySeeder extends Seeder
     }
     function str_replace_json($search, $replace, $subject)
     {
-        return json_decode(str_replace($search, $replace,  json_encode($subject)));
+        return json_decode(str_replace($search, $replace, json_encode($subject)));
     }
 }
