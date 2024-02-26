762d0b4fe77c053f50231ae631615a5c03b8c2ea
DCrepper <zoli.szabok@gmail.com>
2024-02-26 11:30:11 +0100

    Delete unused controllers

--- app/Http/Controllers/ManagePageContentController.php
+++ app/Http/Controllers/ManagePageContentController.php
@@ -27,60 +27,6 @@ class ManagePageContentController extends Controller
         return view('index', compact('categories'));
     }
 
-    public function test()
-    {
-        $this->client =
-            new Client([
-                'base_uri' => UrlPath::BASEURL,
-            ]);
-        $response =
-            $this->client->get(EndPoint::DESIGFORMS);
-        $designFormstmp = json_decode($response->getBody(), true);
-        $designForms = collect($designFormstmp)->first();
-        //dd($designFormstmp[0]['_links']['wp:attachment'][0]['href']);
-        $imgUrl =
-            $this->client->get($designFormstmp[0]['_links']['wp:attachment'][0]['href']);
-        $imgUrl = json_decode($imgUrl->getBody());
-        $imgUrl = collect($imgUrl)->first();
-        dd($imgUrl);
-
-        return view('test', compact('designForms'));
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function blog()
-    {
-        $this->client =
-            new Client([
-                'base_uri' => UrlPath::BASEURL,
-            ]);
-        $category_name = 'Glass';
-        $response = $this->client->get(EndPoint::CATEGORIES);
-        $categories = json_decode($response->getBody());
-        $category = collect($categories)->firstWhere('name', $category_name);
-        $category_id = $category->id;
-
-        $wpApiData = Cache::remember('wpApiData', now()->addDay(), function () {
-            $response = $this->client->get(EndPoint::PAGES.PostPageIds::MAINPAGE);
-            $data = $response->getBody()->getContents();
-            $page = json_decode($data, true);
-
-            return $page;
-        });
-
-        $assets = Cache::remember('assets', now()->addDay(), function () use ($category_id) {
-            $response = $this->client->get(EndPoint::POSTS.'?categories='.$category_id);
-            $data = $response->getBody()->getContents();
-            $assets = json_decode($data, true);
-
-            return $assets;
-        });
-
-        return view('blog', compact('wpApiData', 'assets'));
-    }
-
     public function favorites(Request $request)
     {
         $favoriteProductIds = json_decode(Cookie::get('favorites'));
@@ -125,45 +71,5 @@ class ManagePageContentController extends Controller
         $filePath = storage_path('app/axelpro_exp_items_.xml');
 
         return response()->file($filePath);
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
     }
 }
