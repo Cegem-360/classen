6e2af4c7d6004ac9002c1b6290d602b946b6223e
DCrepper <zoli.szabok@gmail.com>
2024-02-26 13:30:01 +0100

    Refactor prd-carousel.blade.php

--- resources/views/components/partials/prd-carousel.blade.php
+++ resources/views/components/partials/prd-carousel.blade.php
@@ -1,28 +1,35 @@
 @props(['categories' => []])
 
-    <div class="mx-auto flex">
-        <!-- Nav PREV -->
-        <div class="w-fit flex items-center justify-around">
-            <div class="prd-swiper-button-prev cursor-pointer p-4 bg-white rounded-full"><svg style="transform: scaleX(-1);" class="inline" width="24" height="24" viewBox="0 0 96 96" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
-                    <rect id="Artboard1" x="0" y="0" width="96" height="96" style="fill:none;" />
-                    <g id="Artboard11" serif:id="Artboard1">
-                        <g transform="matrix(-1.01053,0,0,1.01053,98.5263,-2.52632)">
-                            <path d="M40.6,88.1L50.2,78.4L28.6,56.8L97.5,56.8L97.5,43.2L28.6,43.2L50.2,21.6L40.6,11.9L2.5,50L40.6,88.1Z" style="fill:rgb(var(--color-szurke));fill-rule:nonzero;" />
-                        </g>
+<div class="mx-auto flex">
+    <!-- Nav PREV -->
+    <div class="flex w-fit items-center justify-around">
+        <div class="prd-swiper-button-prev cursor-pointer rounded-full bg-white p-4"><svg class="inline"
+                style="transform: scaleX(-1);"
+                style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;" width="24"
+                height="24" viewBox="0 0 96 96" version="1.1" xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/">
+                <rect id="Artboard1" style="fill:none;" x="0" y="0" width="96" height="96" />
+                <g id="Artboard11" serif:id="Artboard1">
+                    <g transform="matrix(-1.01053,0,0,1.01053,98.5263,-2.52632)">
+                        <path style="fill:rgb(var(--color-szurke));fill-rule:nonzero;"
+                            d="M40.6,88.1L50.2,78.4L28.6,56.8L97.5,56.8L97.5,43.2L28.6,43.2L50.2,21.6L40.6,11.9L2.5,50L40.6,88.1Z" />
                     </g>
-                </svg></div>
-        </div>
+                </g>
+            </svg></div>
+    </div>
 
-        <div class="swiper prd-swiper w-3/4 sm:w-full h-[50vh]">
-            <div class="swiper-wrapper">
-                @foreach ($categories as $category)
-                    <div class="swiper-slide bg-cover bg-center" style="background-position: 40% 50%; background-image: url('{{ $category->img_url }}');">
-                        <a class="absolute inset-0 flex items-center justify-center bg-[#00000033]" href="{{ route('category.show', ['category' => $category['name']]) }}" wire:navigate.hover>
-                            <h3 class="text-white text-2xl font-bold drop-shadow-md">{{ $category['name'] }}</h3>
-                        </a>
-                    </div>
-                @endforeach
-                {{-- <div class="swiper-slide bg-cover bg-center" style="background-position: 40% 50%; background-image: url('{{ Vite::asset('resources/img/drzwi-przesuwne2-1920x768.webp') }}');">
+    <div class="swiper prd-swiper h-[50vh] w-3/4 sm:w-full">
+        <div class="swiper-wrapper">
+            @foreach ($categories as $category)
+                <div class="swiper-slide bg-cover bg-center"
+                    style="background-position: 40% 50%; background-image: url('{{ $category->img_url }}');">
+                    <a class="absolute inset-0 flex items-center justify-center bg-[#00000033]"
+                        href="{{ route('category.show', ['category' => $category->name]) }}" wire:navigate>
+                        <h3 class="text-2xl font-bold text-white drop-shadow-md">{{ $category->name }}</h3>
+                    </a>
+                </div>
+            @endforeach
+            {{-- <div class="swiper-slide bg-cover bg-center" style="background-position: 40% 50%; background-image: url('{{ Vite::asset('resources/img/drzwi-przesuwne2-1920x768.webp') }}');">
                 </div>
                 <div class="swiper-slide bg-cover bg-center" style="background-position: 40% 100%; background-image: url('{{ Vite::asset('resources/img/classen_china.webp') }}');">
                 </div>
@@ -38,24 +45,27 @@
                 </div>
                 <div class="swiper-slide bg-cover bg-center" style="background-position: 40% 100%; background-image: url('{{ Vite::asset('resources/img/classen_china.webp') }}');">
                 </div> --}}
-            </div>
-            <div class="swiper-pagination prd-swiper-pagination"></div>
         </div>
-
-        <!-- Nav NEXT -->
-        <div class="w-fit flex items-center justify-around">
-            <div class="prd-swiper-button-next cursor-pointer p-4 bg-white rounded-full"><svg class="inline" width="24" height="24" viewBox="0 0 96 96" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
-                    <rect id="Artboard1" x="0" y="0" width="96" height="96" style="fill:none;" />
-                    <g id="Artboard11" serif:id="Artboard1">
-                        <g transform="matrix(-1.01053,0,0,1.01053,98.5263,-2.52632)">
-                            <path d="M40.6,88.1L50.2,78.4L28.6,56.8L97.5,56.8L97.5,43.2L28.6,43.2L50.2,21.6L40.6,11.9L2.5,50L40.6,88.1Z" style="fill:rgb(var(--color-szurke));fill-rule:nonzero;" />
-                        </g>
-                    </g>
-                </svg></div>
-        </div>
-
+        <div class="swiper-pagination prd-swiper-pagination"></div>
     </div>
 
+    <!-- Nav NEXT -->
+    <div class="flex w-fit items-center justify-around">
+        <div class="prd-swiper-button-next cursor-pointer rounded-full bg-white p-4"><svg class="inline"
+                style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;" width="24"
+                height="24" viewBox="0 0 96 96" version="1.1" xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/">
+                <rect id="Artboard1" style="fill:none;" x="0" y="0" width="96" height="96" />
+                <g id="Artboard11" serif:id="Artboard1">
+                    <g transform="matrix(-1.01053,0,0,1.01053,98.5263,-2.52632)">
+                        <path style="fill:rgb(var(--color-szurke));fill-rule:nonzero;"
+                            d="M40.6,88.1L50.2,78.4L28.6,56.8L97.5,56.8L97.5,43.2L28.6,43.2L50.2,21.6L40.6,11.9L2.5,50L40.6,88.1Z" />
+                    </g>
+                </g>
+            </svg></div>
+    </div>
+
+</div>
 
 {{--
 <ul>
