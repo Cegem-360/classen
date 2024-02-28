50769e89a1161a38122b6c25bcf8a4c033f8d034
DCrepper <zoli.szabok@gmail.com>
2024-02-28 15:18:48 +0100

    Change schedule to run daily at midnight

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
     {
         //$schedule->command('inspire')->hourly();
         $schedule->command('queue:work --stop-when-empty')
-            ->everySecond()
+            ->dailyAt('00:00')
             ->withoutOverlapping();
     }
 
