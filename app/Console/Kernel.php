df4a9ffb645929c141612e099ea548560cd28dd4
DCrepper <zoli.szabok@gmail.com>
2024-02-28 15:17:58 +0100

    Change schedule to run every second instead of daily at midnight

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
     {
         //$schedule->command('inspire')->hourly();
         $schedule->command('queue:work --stop-when-empty')
-            ->dailyAt('00:00')
+            ->everySecond()
             ->withoutOverlapping();
     }
 
