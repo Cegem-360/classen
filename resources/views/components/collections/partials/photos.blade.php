ce012c3a94029388b3bfdc48ba4181af39b8006b
Cegem-360 <144362694+Cegem-360@users.noreply.github.com>
2024-02-29 14:26:25 +0100

    door frames photo lightbox

--- resources/views/components/collections/partials/photos.blade.php
+++ resources/views/components/collections/partials/photos.blade.php
@@ -1,12 +1,14 @@
 @props(['photos'])
-<div class="container mx-auto">
-    <div class="grid grid-cols-6 gap-4 lg:grid-cols-3 md:grid-cols-2">
+<div class="mx-8">
+    <div class="grid grid-cols-4 gap-4 lg:grid-cols-3 md:grid-cols-2">
         @foreach ($photos as $photo)
             <div class="relative">
                 <img class="h-full w-full object-cover" src="{{ $photo }}" alt="">
-                <div class="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50">
-                    <a class="text-lg font-bold text-white" href="{{ $photo }}" target="_blank">View</a>
-                </div>
+                <a href="{{ $photo }}" target="_blank" class="group absolute inset-0 flex items-center justify-center transition duration-300 bg-black bg-opacity-5 hover:bg-opacity-40">
+                    <div class="text-lg font-bold text-white opacity-0 group-hover:opacity-100">
+                        <x-svg.plus-icon></x-svg.plus-icon>
+                    </div>
+                </a>
             </div>
         @endforeach
     </div>
---
+++ resources/views/components/svg/plus-icon.blade.php
@@ -0,0 +1,14 @@
+<svg width="40" height="40" viewBox="0 0 40 40" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+    <rect id="Artboard1" x="0" y="0" width="40" height="40" style="fill:none;"/>
+    <clipPath id="_clip1">
+        <rect id="Artboard11" serif:id="Artboard1" x="0" y="0" width="40" height="40"/>
+    </clipPath>
+    <g clip-path="url(#_clip1)">
+        <g transform="matrix(1.63891,0,0,1.77559,-11.7783,-3.72872)">
+            <rect x="18.78" y="2.1" width="1.22" height="22.528" style="fill:white;"/>
+        </g>
+        <g transform="matrix(1.00354e-16,-1.63891,1.77559,1.08724e-16,-3.72872,51.7783)">
+            <rect x="18.78" y="2.1" width="1.22" height="22.528" style="fill:white;"/>
+        </g>
+    </g>
+</svg>
