e6f57829254422de4b447c3afda01bc0e7565adc
DCrepper <zoli.szabok@gmail.com>
2024-03-05 16:20:46 +0100

    Update qtip import

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,7 +8,7 @@ import 'flowbite';
 import { initSwiperJS } from './swiperJS.js';
 import { initFavorites } from './favorites.js';
 import { initCustomJS } from './customJS.js';
-import qtip from 'qtip2';
+import qtip from 'qtip';
 document.addEventListener('livewire:init', () => {
     initSwiperJS();
     initFavorites();
