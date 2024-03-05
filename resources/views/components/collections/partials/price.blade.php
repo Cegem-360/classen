d74ba55ba196edc82cede6e2796234969d0ac2f4
DCrepper <zoli.szabok@gmail.com>
2024-03-05 15:33:50 +0100

    Update currency formatting in price.blade.php

--- resources/views/components/collections/partials/price.blade.php
+++ resources/views/components/collections/partials/price.blade.php
@@ -1,2 +1,2 @@
 @use('Illuminate\Support\Number')
-<span> {{ Number::currency(intval($slot->__toString()), in: 'HUF', locale: 'hu') }} Ft</span>
+<span> {{ Number::currency(intval($slot->__toString()), locale: 'hu', precision: 0) }} Ft</span>
