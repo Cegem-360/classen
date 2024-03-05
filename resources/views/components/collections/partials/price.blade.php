62ec24132492c5055f996885a3df2f6ffdc6748a
DCrepper <zoli.szabok@gmail.com>
2024-03-05 15:36:10 +0100

    Fix currency format in price.blade.php

--- resources/views/components/collections/partials/price.blade.php
+++ resources/views/components/collections/partials/price.blade.php
@@ -1,2 +1,2 @@
 @use('Illuminate\Support\Number')
-<span> {{ Number::currency(intval($slot->__toString()), in: Ft, locale: 'hu') }} Ft</span>
+<span> {{ Number::currency(intval($slot->__toString()), in: ' Ft', locale: 'hu') }} Ft</span>
