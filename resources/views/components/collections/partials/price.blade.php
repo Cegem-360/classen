99cb086a49722b254d02edb7176e8fb67e99aa87
DCrepper <zoli.szabok@gmail.com>
2024-03-05 15:40:03 +0100

    Refactor price formatting in price.blade.php

--- resources/views/components/collections/partials/price.blade.php
+++ resources/views/components/collections/partials/price.blade.php
@@ -1,2 +1,2 @@
 @use('Illuminate\Support\Number')
-<span> {{ Number::currency(intval($slot->__toString()), in: ' Ft', locale: 'hu', precision: 0) }} Ft</span>
+<span> {{ Number::format(intval($slot->__toString()), locale: 'hu') }} Ft</span>
