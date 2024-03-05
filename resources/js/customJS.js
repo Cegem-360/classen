a8f79d8c91542f31bbaf1f143bd86b21e9482643
DCrepper <zoli.szabok@gmail.com>
2024-03-05 16:23:47 +0100

    Update JS files and manifest.json

--- resources/js/customJS.js
+++ resources/js/customJS.js
@@ -2,7 +2,7 @@ export function initCustomJS() {
 
     function selectColor(obj) {
         var target = document.getElementById(obj);
-        $('.door_variant.active').removeClass('active');
+        document.getElementsByClassName('color_variant active')[0].classList.remove('active');
         target.classList.add('active');
     }
 }
