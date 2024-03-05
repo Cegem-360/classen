7a5eab152d6e30bf28375dc6bf7362812ca9af25
DCrepper <zoli.szabok@gmail.com>
2024-03-05 15:35:09 +0100

    Update currency locale in price.blade.php

--- resources/views/components/collections/partials/price.blade.php
+++ resources/views/components/collections/partials/price.blade.php
@@ -1,2 +1,2 @@
 @use('Illuminate\Support\Number')
-<span> {{ Number::currency(intval($slot->__toString()), locale: 'hu') }} Ft</span>
+<span> {{ Number::currency(intval($slot->__toString()), locale: 'huf') }} Ft</span>
