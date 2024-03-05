afd46482c8a0d79ffceebef3c71fce2d5ca74e6b
DCrepper <zoli.szabok@gmail.com>
2024-03-05 16:20:09 +0100

    Update manifest.json and app.js files

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,7 @@ import 'flowbite';
 import { initSwiperJS } from './swiperJS.js';
 import { initFavorites } from './favorites.js';
 import { initCustomJS } from './customJS.js';
+import qtip from 'qtip2';
 document.addEventListener('livewire:init', () => {
     initSwiperJS();
     initFavorites();
