87f6f4898095c3c1d1f7f52a2b0079da2e1677e6
Cegem-360 <144362694+Cegem-360@users.noreply.github.com>
2024-03-07 11:03:29 +0100

    door-model-selector NO SWIPER

--- resources/views/components/collections/partials/door-model-selector.blade.php
+++ resources/views/components/collections/partials/door-model-selector.blade.php
@@ -1,40 +1,40 @@
    @props(['category' => '', 'modelVariants' => [], 'door' => ''])
-   <div class="mx-8 grid grid-cols-2 gap-3 md:grid-cols-1">
-       <div class="swiper door-model-swiper w-full">
-           <div class="swiper-wrapper max-h-[500px] bg-neutral-200 p-12">
-               @foreach ($modelVariants as $model)
-                   <div class="swiper-slide !flex justify-center sm:w-full">
-                       <a href="{{ route('door.show', ['door' => $model->id]) }}" wire:navigate>
-                           <img class="max-h-[500px]"
-                               src="{{ $model->img_url ?? Vite::asset('resources/img/placeholder.webp') }}" />
-                       </a>
-                   </div>
-               @endforeach
-           </div>
-       </div>
-       <div>
-           <div class="mb-6 pl-8">
-               <h2 class="mb-3 text-4xl font-bold">{{ $door->name }}</h2>
-               <hr class="relative mb-3 mt-3 h-1 w-32 border-0 bg-sarga">
-               <h3 class="mb-3 text-lg">{{ __('Collection: ') }}<span class="font-bold">{{ $category->name }}</span>
-               </h3>
-               <x-collections.partials.price
-                   class="text-lg font-bold">{{ $door->price }}</x-collections.partials.price>
-               <form action="{{ route('quotation.add', ['door' => $door->id]) }}" method="post">
-                   @csrf
-                   <x-primary-button class="mt-6 w-fit" type="submit">{{ __('Add to Request a Quote') }}
-                   </x-primary-button>
-               </form>
-           </div>
-           <div class="swiper door-model-swiper-thumbs w-full" thumbsSlider="">
-               <div class="swiper-wrapper flex-wrap">
-                   @foreach ($modelVariants as $model)
-                       <div class="swiper-slide bg-neutral-200 p-6 pb-2">
-                           <img src="{{ $model->img_url ?? Vite::asset('resources/img/placeholder.webp') }}" />
-                           <div class="desc w-full pt-2">{{ $model->name }}</div>
-                       </div>
-                   @endforeach
-               </div>
-           </div>
-       </div>
-   </div>
+ <div class="mx-8 grid grid-cols-2 gap-3 md:grid-cols-1">
+     <div class="door-model-swiper w-full">
+         <div class="bg-neutral-200 p-12">
+              {{-- @foreach ($modelVariants as $model)
+              <div class="swiper-slide !flex justify-center sm:w-full">
+                  <img class="max-h-[500px]" src="{{ $model->img_url ?? Vite::asset('resources/img/placeholder.webp') }}" />
+              </div>
+             @endforeach --}}
+             <div class="!flex justify-center sm:w-full">
+                 <img class="max-h-[500px]" src="{{ $door->img_url ?? Vite::asset('resources/img/placeholder.webp') }}" />
+             </div>
+         </div>
+     </div>
+     <div>
+         <div class="mb-6 pl-8">
+             <h2 class="mb-3 text-4xl font-bold">{{ $door->name }}</h2>
+             <hr class="relative mb-3 mt-3 h-1 w-32 border-0 bg-sarga">
+             <h3 class="mb-3 text-lg">{{ __('Collection: ') }}<span class="font-bold">{{ $category->name }}</span>
+             </h3>
+             <x-collections.partials.price class="text-lg font-bold">{{ $door->price }}</x-collections.partials.price>
+             <form action="{{ route('quotation.add', ['door' => $door->id]) }}" method="post">
+                 @csrf
+                 <x-primary-button class="mt-6 w-fit" type="submit">{{ __('Add to Request a Quote') }}</x-primary-button>
+             </form>
+         </div>
+         <div class="door-model-swiper-thumbs w-full" thumbsSlider="">
+             <div class="grid grid-cols-4 gap-3">
+                 @foreach ($modelVariants as $model)
+                 <div class="bg-neutral-200 p-6 pb-2">
+                     <a href="{{ route('door.show', ['door' => $model->id]) }}" wire:navigate>
+                         <img src="{{ $model->img_url ?? Vite::asset('resources/img/placeholder.webp') }}" />
+                     </a>
+                     <div class="desc w-full pt-2">{{ $model->name }}</div>
+                 </div>
+                 @endforeach
+             </div>
+         </div>
+     </div>
+ </div>
