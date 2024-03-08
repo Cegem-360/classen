2c846830e3485f2d42ed11e7b4a7f9c829754a06
DCrepper <zoli.szabok@gmail.com>
2024-03-08 10:15:02 +0100

    Update navigation styling and search box layout

--- resources/views/livewire/search-box.blade.php
+++ resources/views/livewire/search-box.blade.php
@@ -1,64 +1,18 @@
-<div>
-    <form class="flex items-center border-[1px] border-[#000] md:hidden" wire.subbmit="fetchCategory">
-        <div>
-            <!-- CSS -->
-            <style type="text/css">
-                .search-box .clear {
-                    clear: both;
-                    margin-top: 20px;
-                }
-
-                .search-box ul {
-                    list-style: none;
-                    padding: 0px;
-                    width: 250px;
-                    position: absolute;
-                    margin: 0;
-                    background: white;
-                }
-
-                .search-box ul li {
-                    background: lavender;
-                    padding: 4px;
-                    margin-bottom: 1px;
-                }
-
-                .search-box ul li:nth-child(even) {
-                    background: cadetblue;
-                    color: white;
-                }
-
-                .search-box ul li:hover {
-                    cursor: pointer;
-                }
-
-                .search-box input[type=text] {
-                    padding: 5px;
-                    width: 250px;
-                    letter-spacing: 1px;
-                }
-            </style>
-
-            <div class="search-box">
-                <input type='text' wire:model.live="search" wire:keyup="searchResult">
-                <button class="flex-auto pr-2">
-                    <x-svg.search />
-                </button>
-                <!-- Search result list -->
-                @if ($showdiv)
-                    <ul>
-                        @if (!empty($records))
-                            @foreach ($records as $record)
-                                <li wire:click="fetchCategory({{ $record->id }})">{{ $record->name }}</li>
-                            @endforeach
-                        @endif
-                    </ul>
+    <form class="relative flex items-center border-[1px] border-[#000] md:hidden" wire.subbmit="fetchCategory">
+        <input class="border-0 active:border-none text-xs" type="text" placeholder="Keresés" wire:model.live="search" wire:keyup="searchResult">
+        <button class="flex-auto pr-2">
+            <x-svg.search />
+        </button>
+        <div class="search-box mt-3">
+            <!-- Search result list -->
+            @if ($showdiv)
+            <ul class="absolute left-0 top-9 list-none m-0 p-0 w-56 bg-white">
+                @if (!empty($records))
+                @foreach ($records as $record)
+                <li class="px-3 py-1 bg-[#f4f4f4] even:bg-[#e8e8e8] hover:cursor-pointer" wire:click="fetchCategory({{ $record->id }})">{{ $record->name }}</li>
+                @endforeach
                 @endif
-
-                <div class="clear"></div>
-
-            </div>
-
+            </ul>
+            @endif
         </div>
     </form>
-</div>
