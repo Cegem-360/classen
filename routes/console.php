72bceb60cb94de87feafdfd025e1d90a4d8b1b81
Szabó Zoltán <zoli.szabok@gmail.com>
2024-05-03 11:52:55 +0200

    Refactor cache clearing command in routes/console.php

--- routes/console.php
+++ routes/console.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
-Artisan::command('clear cache', function () {
+Artisan::command('clearCache', function () {
     Artisan::call('cache:clear');
     Artisan::call('config:clear');
     Artisan::call('route:clear');
