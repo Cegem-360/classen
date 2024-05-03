b5b10cb9afa7d942afc919a9f0f839507c483945
Szabó Zoltán <zoli.szabok@gmail.com>
2024-05-03 11:52:32 +0200

    Add command to clear cache in routes/console.php

--- routes/console.php
+++ routes/console.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+Artisan::command('clear cache', function () {
+    Artisan::call('cache:clear');
+    Artisan::call('config:clear');
+    Artisan::call('route:clear');
+    Artisan::call('view:clear');
+    $this->info('Cache cleared');
+})->purpose('Clear cache');
