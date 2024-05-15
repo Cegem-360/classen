bbb003ad9cb0d580a0fa8673355a7dcf2412d791
Szabó Zoltán <zoli.szabok@gmail.com>
2024-05-15 12:04:17 +0200

    Update door color variant selector styling and lazy loading

--- resources/views/components/collections/partials/door-color-variant-selector.blade.php
+++ resources/views/components/collections/partials/door-color-variant-selector.blade.php
@@ -2,21 +2,22 @@
 <!-- Color variant -->
 <div class="mx-8">
     <h2 class="mb-3 text-4xl font-bold">Színvariációk</h2>
-    <hr class="relative mb-3 mt-3 h-1 w-32 border-0 bg-sarga">
+    <hr class="relative w-32 h-1 mt-3 mb-3 border-0 bg-sarga">
     <div class="min-h-[30px]"></div>
 
     <div class="grid grid-cols-2 gap-8 bg-neutral-100 sm:grid-cols-1">
-        <div class="doors__col doors__fronts p-8" id="doors__fronts">
+        <div class="p-8 doors__col doors__fronts" id="doors__fronts">
             <div class="door_fronts">
-                <div class="swiper lightbox3 door-color-variant-swiper w-full">
-                    <div class="swiper-wrapper items-center">
+                <div class="w-full swiper lightbox3 door-color-variant-swiper">
+                    <div class="items-center swiper-wrapper">
                         @foreach ($colorVariants as $colorVariant)
                             @foreach ($colorVariant as $variant)
                                 <div class="swiper-slide" data-hash="{{ $variant->tag }}">
-                                    {{-- <a data-pswp-width="540" data-pswp-height="1200" href="{{ $variant->img_url ?? Vite::asset('resources/img/placeholder.webp') }}" itemprop="contentUrl" target="_blank"> --}}
-                                        <div class="flex h-full w-full justify-center bg-[#ffffff00]">
-                                            <img class="max-h-[500px]" src="{{ $variant->img_url ?? Vite::asset('resources/img/placeholder.webp') }}" alt="" loading="lazy" itemprop="contentUrl" target="_blank">
-                                        </div>
+                                    <div class="flex h-full w-full justify-center bg-[#ffffff00]">
+                                        <img class="max-h-[500px]"
+                                            src="{{ $variant->img_url ?? Vite::asset('resources/img/placeholder.webp') }}"
+                                            alt="" loading="lazy" itemprop="contentUrl" target="_blank">
+                                    </div>
                                     {{-- </a> --}}
                                 </div>
                             @endforeach
@@ -25,7 +26,7 @@
                 </div>
             </div>
         </div>
-        <div class="doors__col doors__colors px-4 py-16">
+        <div class="px-4 py-16 doors__col doors__colors">
             <div class="door_swatches">
                 <div class="min-h-[330px] flex-wrap items-center md:flex-col">
                     @foreach ($colorVariants as $category => $colors)
@@ -33,14 +34,14 @@
                             <h3 class="text-2xl font-bold">{{ __($category) }}</h3>
                             <div class="doors_colors__variant-set flex flex-wrap border-b-1 border-[#bfbfbf] py-8">
                                 @foreach ($colors as $color)
-                                    <div class="door_variant mx-4 my-2" id='{{ $color->tag }}-color'
+                                    <div class="mx-4 my-2 door_variant" id='{{ $color->tag }}-color'
                                         onclick="selectColor('{{ $color->tag }}-color')">
                                         <a class="block w-[94px]" href="#{{ $color->tag }}">
                                             <img class="h-[88px] w-[88px] rounded-full"
                                                 src="{{ $color->tag_img_url ?? Vite::asset('resources/img/placeholder.webp') }}"
                                                 loading="lazy">
                                             <div class="door_variant__meta">
-                                                <h4 class="mt-2 text-center text-sm">{{ __($color->tag_name) }}</h4>
+                                                <h4 class="mt-2 text-sm text-center">{{ __($color->tag_name) }}</h4>
                                             </div>
                                         </a>
                                     </div>
--- resources/views/doors/show.blade.php
+++ resources/views/doors/show.blade.php
@@ -1,4 +1,13 @@
 <x-layouts.app>
+    <x-slot name="meta">
+        <meta name="robots" content="index, follow">
+        <meta name="googlebot" content="index, follow">
+        <meta name="description"
+            content="Az Arcadia98 Kft. Ajtók aloldala bemutatja {{ $category->name }} {{ $door->name }}, részletes információkat nyújt a moddelről. Fedezze fel kínálatunkat, és válasszon az Önnek legmegfelelőbb ajtók közül!">
+        <meta name="keywords"
+            content="ajtók, egyedi modellek, kínálat, szűrés, Arcadia98 Kft., {{ $category->name }}, {{ $door->name }}">
+        <title>{{ strtoupper($door->name) }} - {{ strtoupper($category->name) }} - Arcadia98 Kft. </title>
+    </x-slot>
     <x-nav.breadcrumb>
         {{ $category->name }}
     </x-nav.breadcrumb>
