740be88a22e3d627352bbd7e2e391bb103dd5c49
Szabó Zoltán <zoli.szabok@gmail.com>
2024-06-05 12:44:21 +0200

    refactor: Update meta tags and canonical link for quotation success page

--- resources/views/quotation/success.blade.php
+++ resources/views/quotation/success.blade.php
@@ -1,6 +1,15 @@
 <x-layouts.app>
+    <x-slot name="meta">
+        <meta name="robots" content="index, nofollow">
+        <meta name="googlebot" content="index, nofollow">
+
+        <meta name="description"
+            content="Sikeres árajánlat tétel leadása megtötént és email visszaigazolás elküldése megtörtént az árajánlatról.">
+        <meta name="keywords" content="sikeres árajánlat tétel leadása, sikeres leadás, árajánlat köszönő oldal">
+        <title>Sikeres árajánlat kérés - Arcadia98 Kft.</title>
+    </x-slot>
     <x-nav.breadcrumb>
-        Árajánlat kérés
+        Sikeres árajánlat kérés
     </x-nav.breadcrumb>
 
     <!-- gap -->
@@ -9,7 +18,7 @@
     <div class="mx-16 md:mx-8 sm:mx-4">
         <div class="mb-16">
             <h2 class="mb-3 text-4xl font-bold">Árajánlat kérés</h2>
-            <hr class="relative mb-3 mt-3 h-1 w-32 border-0 bg-sarga">
+            <hr class="relative w-32 h-1 mt-3 mb-3 border-0 bg-sarga">
         </div>
         <div class="container">
             <div class="row">
