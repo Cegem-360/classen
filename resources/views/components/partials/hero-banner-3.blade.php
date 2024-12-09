3d42a65df9726ecd47b5919d94fec59b74c97a78
Cegem-360 <144362694+Cegem-360@users.noreply.github.com>
2024-12-09 11:30:48 +0100

    Karacsonyi valtozasok

--- resources/views/components/partials/hero-banner-3.blade.php
+++ resources/views/components/partials/hero-banner-3.blade.php
@@ -1,5 +1,6 @@
 @use('App\Models\WebsiteOptions')
-<div class="mx-auto flex h-1/2 flex-wrap items-center !grid grid-cols-[1fr_3fr] sm:grid-cols-1 bg-gradient-to-b from-[#978f88] to-[#c4c0bd] text-white">
+<div
+    class="mx-auto flex h-1/2 flex-wrap items-center !grid grid-cols-[1fr_3fr] sm:grid-cols-1 bg-gradient-to-b from-[#978f88] to-[#c4c0bd] text-white">
 
     <div class="relative h-[80vh] sm:h-fit">
         @foreach (WebsiteOptions::where('name', 'fooldal_hero_banner_kepek')->get() as $item)
@@ -31,6 +32,12 @@
 
     <div class="swiper hero-swiper relative h-[80vh] w-full sm:h-fit">
         <div class="swiper-wrapper">
+            <div class="swiper-slide">
+                <!-- Hero Image -->
+                <div class="hero-image h-[80vh] bg-cover bg-center sm:h-[100vh]"
+                    style="background-position: 40% 50%; background-image: url('http://admin.classen.cegem360.hu/wp-content/uploads/2024/12/arcadia-karacsony.webp');">
+                </div>
+            </div>
             @foreach (WebsiteOptions::where('name', 'fooldal_hero_banner_kepek')->get() as $item)
                 <div class="swiper-slide">
                     <!-- Hero Image -->
