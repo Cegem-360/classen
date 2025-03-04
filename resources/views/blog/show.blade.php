4979a28fdf7df588afa892dd5e6d658b40414e18
Cegem-360 <144362694+Cegem-360@users.noreply.github.com>
2025-03-04 18:55:45 +0100

    blog fix

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -43,8 +43,8 @@
             <div class="container px-8 mx-auto">
                 <div class="grid grid-cols-2 sm:grid-cols-1 gap-8 mb-16">
                     {{-- <h1 class="gap-40 my-11">{{ $blog?->title }}</h1> --}}
-                    {{-- <div>{!! str($blog?->content)->sanitizeHtml() !!}</div> --}}
-                    <div>{{ $blog?->content }}</div>
+                    <div>{!! str($blog?->content)->sanitizeHtml() !!}</div>
+                    {{-- <div>{{ $blog?->content }}</div> --}}
                     <img src="{{ $blog?->img_url }}" alt="{{ $blog?->title }}"
                         class="sm:order-1 w-fit object-cover aspect-4/3">
                 </div>
