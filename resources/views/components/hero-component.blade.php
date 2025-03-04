12e8954a3183c537482bb6e919f9000c2d1772bf
Cegem-360 <144362694+Cegem-360@users.noreply.github.com>
2025-03-05 00:04:04 +0100

    blog show fix

--- resources/views/components/hero-component.blade.php
+++ resources/views/components/hero-component.blade.php
@@ -14,7 +14,7 @@
     <img src="{{ $image }}" alt=""
         class="{{ $imgPosition }} absolute inset-0 w-full h-full object-cover z-0 brightness-[.9]">
     <div class="absolute inset-0 w-full h-full z-[1] bg-gradient-to-b from-[#00000000] to-[#000000d0]"></div>
-    <h2 class="relative sm:max-w-[70%] sm:text-3xl text-5xl text-center drop-shadow z-10">{{ __($title) }}
+    <h2 class="relative max-w-[50%] sm:max-w-[90%] sm:text-3xl text-5xl text-center drop-shadow z-10">{{ __($title) }}
     </h2>
     @if ($description)
         <p class="relative max-w-[70%] sm:text-xl text-2xl text-center drop-shadow z-10">
