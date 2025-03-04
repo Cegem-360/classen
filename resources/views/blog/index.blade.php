67494eeaecf58d69055d7cb2298899974c9352b4
Cegem-360 <144362694+Cegem-360@users.noreply.github.com>
2025-03-04 23:52:02 +0100

    blog index refactor

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -10,24 +10,24 @@
             @foreach ($blogs as $blog)
                 {{-- @dump($blog) --}}
 
-                <div class="p-4 sm:mb-0 mb-6">
-                    <div class="rounded-lg h-64 overflow-hidden">
-                        <a href="{{ route('blog.show', ['slug' => $blog->slug]) }}"><img alt="content"
-                                class="object-cover object-center h-full w-full" src="{{ $blog->img_url }}"></a>
-                    </div>
-                    <a href="{{ route('blog.show', ['slug' => $blog->slug]) }}">
+                <a href="{{ route('blog.show', ['slug' => $blog->slug]) }}" class="group">
+                    <div class="p-4 sm:mb-0 mb-6">
+                        <div class="rounded-lg h-64 overflow-hidden">
+                            <img alt="{{ $blog->title }}" class="object-cover object-center h-full w-full"
+                                src="{{ $blog->img_url }}">
+                        </div>
                         <h2 class="text-xl font-medium title-font text-gray-900 mt-5">{{ $blog->title }}</h2>
-                    </a>
-                    <p class="text-base leading-relaxed mt-2">{{ Str::limit(strip_tags($blog->content), 150, '...') }}
-                    </p>
-                    <a href="{{ route('blog.show', ['slug' => $blog->slug]) }}"
-                        class="text-black font-bold inline-flex items-center mt-3">{{ __('Elolvasom') }}
-                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                            stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                            <path d="M5 12h14M12 5l7 7-7 7"></path>
-                        </svg>
-                    </a>
-                </div>
+                        <p class="text-base leading-relaxed mt-2">
+                            {{ Str::limit(strip_tags($blog->content), 150, '...') }}</p>
+                        <button
+                            class="text-black border-b-3 border-transparent group-hover:border-sarga transition duration-300 font-bold inline-flex items-center mt-3">Elolvasom
+                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                                <path d="M5 12h14M12 5l7 7-7 7"></path>
+                            </svg>
+                        </button>
+                    </div>
+                </a>
             @endforeach
         </div>
     </div>
