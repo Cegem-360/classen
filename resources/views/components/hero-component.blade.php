732b51dddd5c800904aed3c61e2e91010f2d25a8
Zoltán Tamás Szabó <zoli.szabok@gmail.com>
2025-03-05 14:38:10 +0100

    fix: update image source handling in hero and blog components to use Vite asset helper

--- resources/views/components/hero-component.blade.php
+++ resources/views/components/hero-component.blade.php
@@ -11,7 +11,7 @@
 
 <div
     class="relative flex flex-col gap-8 p-8 justify-center items-center bg-[#C5C5C5] {{ $height }} {{ $width }} text-white">
-    <img src="{{ $image }}" alt=""
+    <img src="{{ Vite::asset('resources/img/' . $image) }}" alt=""
         class="{{ $imgPosition }} absolute inset-0 w-full h-full object-cover z-0 brightness-[.9]">
     <div class="absolute inset-0 w-full h-full z-[1] bg-gradient-to-b from-[#00000000] to-[#000000d0]"></div>
     <h2 class="relative max-w-[50%] sm:max-w-[90%] sm:text-3xl text-5xl text-center drop-shadow z-10">{{ __($title) }}
