7fe5eacc7ede12d41ef8de2e728880fc3da4efb7
Zoltán Tamás Szabó <zoli.szabok@gmail.com>
2025-08-05 13:22:30 +0200

    Add meta tags for SEO optimization in featured sales areas view

--- resources/views/livewire/featured-sales-areas-show.blade.php
+++ resources/views/livewire/featured-sales-areas-show.blade.php
@@ -1,4 +1,22 @@
 <div>
+    <x-slot name="meta">
+
+        <meta name="description" content="{{ $featuredSalesPage->meta_description ?? 'Kiemelt értékesítési területek' }}">
+        <meta name="keywords"
+            content="{{ $featuredSalesPage->meta_keywords ?? 'értékesítési terület, kiemelt, ajánlat' }}">
+        <meta name="author" content="{{ $featuredSalesPage->meta_author ?? 'Cegem360 Kft.' }}">
+        <meta property="og:title" content="{{ $featuredSalesPage->title ?? 'Kiemelt értékesítési területek' }}">
+        <meta property="og:description"
+            content="{{ $featuredSalesPage->meta_description ?? 'Kiemelt értékesítési területek' }}">
+        <meta property="og:image" content="{{ $featuredSalesPage->image_url ?? asset('images/default-featured.jpg') }}">
+        <meta property="og:url" content="{{ request()->url() }}">
+        <meta name="twitter:card" content="summary_large_image">
+        <meta name="twitter:title" content="{{ $featuredSalesPage->title ?? 'Kiemelt értékesítési területek' }}">
+        <meta name="twitter:description"
+            content="{{ $featuredSalesPage->meta_description ?? 'Kiemelt értékesítési területek' }}">
+        <meta name="twitter:image"
+            content="{{ $featuredSalesPage->image_url ?? asset('images/default-featured.jpg') }}">
+    </x-slot>
     @empty($featuredSalesPage)
         <p class="text-gray-500">Nincs elérhető kiemelt értékesítési terület.</p>
     @else
